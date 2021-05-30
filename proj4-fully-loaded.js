ccd608d35f6723acf3589ce9d0a9e76ad4de76e4
DanielJDufour <daniel.j.dufour@gmail.com>
2021-05-29 21:55:12 -0400

    added Angular support

--- proj4-fully-loaded.js
+++ proj4-fully-loaded.js
@@ -1,5 +1,10 @@
-const proj4 = require("proj4");
+let proj4 = require("proj4");
 const defs = require("proj4js-definitions");
+
+if (typeof proj4 === "object" && typeof proj4.defs !== "function" && typeof proj4.default === "function") {
+  // probably inside an Angular project
+  proj4 = proj4.default;
+}
 
 proj4.defs(defs);
 
